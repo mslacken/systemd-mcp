607ca2fec53f160b0ddd48498c792e99630e3c01
Christian Goll <cgoll@suse.com>
2026-01-15 15:16:43 +0100

    removed gettime completely

--- systemd-mcp.go
+++ systemd-mcp.go
@@ -240,35 +240,7 @@ func main() {
 			})
 		},
 	},
-		struct {
-			Tool     *mcp.Tool
-			Register func(server *mcp.Server, tool *mcp.Tool)
-		}{
-			/*
-				Tool: &mcp.Tool{
-					Name:        "get_current_system_time",
-					Description: "Returns the current system time including time zone information in a format compatible with ListLogParams.",
-					InputSchema: nil,
-				},
-			*/
-			Register: func(server *mcp.Server, tool *mcp.Tool) {
-				mcp.AddTool(server, tool, func(ctx context.Context, req *mcp.CallToolRequest, args *struct{}) (*mcp.CallToolResult, any, error) {
-					slog.Debug("get_current_system_time called")
-					currentTime := time.Now()
-					jsonBytes, err := json.Marshal(currentTime.Format(time.RFC3339))
-					if err != nil {
-						return nil, nil, fmt.Errorf("failed to marshal current time: %w", err)
-					}
-					return &mcp.CallToolResult{
-						Content: []mcp.Content{
-							&mcp.TextContent{
-								Text: string(jsonBytes),
-							},
-						},
-					}, currentTime, nil
-				})
-			},
-		})
+	)
 
 	var allTools []string
 	for _, tool := range tools {
